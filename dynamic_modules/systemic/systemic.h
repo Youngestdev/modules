4d055e0463339b29f0dfdcc76488a27c5f0bc0a1
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 22:49:47 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/systemic/systemic.h
+++ dynamic_modules/systemic/systemic.h
@@ -19,8 +19,36 @@
 extern "C" {
 #endif
 
+void simple_vm_os_loadfunctions ( SimpleState *pSimpleState ) ;
 
+void simple_vm_os_ismsdos ( void *pPointer ) ;
 
+void simple_vm_os_iswindows ( void *pPointer ) ;
+
+void simple_vm_os_iswindows64 ( void *pPointer ) ;
+
+void simple_vm_os_isunix ( void *pPointer ) ;
+
+void simple_vm_os_ismacosx ( void *pPointer ) ;
+
+void simple_vm_os_islinux ( void *pPointer ) ;
+
+void simple_vm_os_isfreebsd ( void *pPointer ) ;
+
+void simple_vm_os_isandroid ( void *pPointer ) ;
+
+void simple_vm_os_windowsnl ( void *pPointer ) ;
+
+void simple_vm_os_currentdir ( void *pPointer ) ;
+
+void simple_vm_os_exefilename ( void *pPointer ) ;
+
+void simple_vm_os_chdir ( void *pPointer ) ;
+
+void simple_vm_os_exefolder ( void *pPointer ) ;
+
+/* Constants */
+#define SIMPLE_VM_POINTER_LIBNAME "system"
 
 #ifdef __cplusplus
 }
