409260bf2555776ec74eda6e88f7bb4d54e78b02
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 03:51:46 +0100

    initialize the string_savant Dynamic libary

---
+++ dynamic_modules/mathic/mathic.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mathic.h
+ * Author: thecarisma
+ *
+ * Created on February 9, 2018, 3:50 AM
+ */
+
+#ifndef MATHIC_H
+#define MATHIC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MATHIC_H */
+
