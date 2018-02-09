8989ca77f05f62e8329799c82fcf7c07570afa94
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 21:23:07 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/file_savant/file_savant.c
+++ dynamic_modules/file_savant/file_savant.c
@@ -19,25 +19,6 @@ SIMPLE_API __declspec(dllexport)
 
 SIMPLE_API void init_simple_module(SimpleState *sState)
 {   
-    register_block("sin",math_sin);
-    register_block("cos",math_cos);
-    register_block("an",math_tan);
-    register_block("asin",math_asin);
-    register_block("acos",math_acos);
-    register_block("atan",math_atan);
-    register_block("atan2",math_atan2);
-    register_block("sinh",math_sinh);
-    register_block("cosh",math_cosh);
-    register_block("tanh",math_tanh);
-    register_block("exp",math_exp);
-    register_block("log",math_log);
-    register_block("log10",math_log10);
-    register_block("ceil",math_ceil);
-    register_block("floor",math_floor);
-    register_block("fabs",math_fabs);
-    register_block("pow",math_pow);
-    register_block("sqrt",math_sqrt);
-    register_block("unsigned",math_unsigned);
-    register_block("decimals",math_decimals);
+    //register_block("sin",math_sin);
 }
 
