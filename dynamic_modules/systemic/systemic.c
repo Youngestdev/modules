a00d50d8aa84b26501d0cbb1c766b7434381a51e
Thecarisma <azeezadewale98@gmail.com>
2018-02-10 00:03:11 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/systemic/systemic.c
+++ dynamic_modules/systemic/systemic.c
@@ -36,7 +36,7 @@ SIMPLE_API void init_simple_module(SimpleState *sState)
     register_block("windowsnl",os_windowsnl);
     register_block("currentDirectory",os_currentdir);
     register_block("exefilename",os_exefilename);
-    register_block("changeDir",os_chdir);
+    register_block("changeDirectory",os_chdir);
     register_block("exeFolder",os_exefolder);
     register_block("getSystemEnvironment",get_env);
 }
