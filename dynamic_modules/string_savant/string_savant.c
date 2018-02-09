e29355ea0a8e4b45491ad8edcc88fcb2a20649c2
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 21:13:08 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/string_savant/string_savant.c
+++ dynamic_modules/string_savant/string_savant.c
@@ -15,6 +15,8 @@
 #include "../../../simple/bootsrc/includes/simple.h"
 #include "string_savant.h"
 
+SIMPLE_API __declspec(dllexport)
+
 SIMPLE_API void init_simple_module(SimpleState *sState)
 {
 	register_block("$__left",simple_vmlib_left);
