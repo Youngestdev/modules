790a013e25ba2b861eee19a3b5fbf219514d0ddc
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 21:03:11 +0100

    initialize the string_savant Dynamic libary

---
+++ dynamic_modules/file_savant/file_savant.c
@@ -0,0 +1,25 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2017 simple 
+
+*/
+
+/* 
+ * File:   file_savant.c
+ * Author: thecarisma
+ *
+ * Created on February 9, 2018, 9:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ dynamic_modules/file_savant/file_savant.h
@@ -0,0 +1,30 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2017 simple 
+
+*/
+
+/* 
+ * File:   file_savant.h
+ * Author: thecarisma
+ *
+ * Created on February 9, 2018, 9:02 PM
+ */
+
+#ifndef FILE_SAVANT_H
+#define FILE_SAVANT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FILE_SAVANT_H */
+
