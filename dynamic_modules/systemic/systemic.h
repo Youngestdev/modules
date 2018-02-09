b21c4dc569480627d87c790cb183c27298088961
Thecarisma <azeezadewale98@gmail.com>
2018-02-09 22:04:48 +0100

    initialize the string_savant Dynamic libary

---
+++ dynamic_modules/systemic/systemic.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   systemic.h
+ * Author: thecarisma
+ *
+ * Created on February 9, 2018, 10:04 PM
+ */
+
+#ifndef SYSTEMIC_H
+#define SYSTEMIC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEMIC_H */
+
