6767ba11384b70522e3f6438670acabefaddb434
Thecarisma <azeezadewale98@gmail.com>
2018-02-11 00:29:34 +0100

    initialize the string_savant Dynamic libary

---
+++ dynamic_modules/consoler/consoler.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   consoler.h
+ * Author: thecarisma
+ *
+ * Created on February 11, 2018, 12:28 AM
+ */
+
+#ifndef CONSOLER_H
+#define CONSOLER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONSOLER_H */
+
