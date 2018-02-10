ab9a9ae10b18174f71520b958211f1cf6bc95bd3
Thecarisma <azeezadewale98@gmail.com>
2018-02-10 18:19:19 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/string_savant/string_savant.c
+++ dynamic_modules/string_savant/string_savant.c
@@ -389,7 +389,7 @@ void string_last_string ( void *pointer )
 	} char *filename = (char*)calloc(1, sizeof(SIMPLE_API_ISSTRING(1)));
 	if ( SIMPLE_API_ISSTRING(1) && SIMPLE_API_ISSTRING(2) ) {
             filename = strrchr(SIMPLE_API_GETSTRING(1), SIMPLE_API_GETSTRING(2)) + 1;
-            printf("IN C : %s -> %s\n", SIMPLE_API_GETSTRING(2), filename);
+            //printf("IN C : %s -> %s\n", SIMPLE_API_GETSTRING(2), filename);
             SIMPLE_API_RETSTRING(filename);
 	} else {
 		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
