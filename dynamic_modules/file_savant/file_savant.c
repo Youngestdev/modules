44b73169394853dadfc14d5136a7b432e3a0cb97
Thecarisma <azeezadewale98@gmail.com>
2018-02-10 21:06:00 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/file_savant/file_savant.c
+++ dynamic_modules/file_savant/file_savant.c
@@ -25,6 +25,7 @@ SIMPLE_API void init_simple_module(SimpleState *sState)
     register_block("__exists",file_exists);
     register_block("__rename",file_rename);
     register_block("__delete",file_delete);
+    register_block("blow_dir",file_delete);
 }
 
 void read_file ( void *pointer )
