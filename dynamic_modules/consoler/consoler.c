efb99f3b5bb6108661decf361f576efd42811c8a
Thecarisma <azeezadewale98@gmail.com>
2018-02-11 01:16:14 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/consoler/consoler.c
+++ dynamic_modules/consoler/consoler.c
@@ -21,8 +21,24 @@ SIMPLE_API __declspec(dllexport)
 
 SIMPLE_API void init_simple_module(SimpleState *sState)
 {   
-    register_block("_printwfb",print_with_foreground);
+    register_block("_printwfb",print_with_foreground_background);
 }
 
-
+SIMPLE_BLOCK(print_with_foreground_background)
+{
+	if ( SIMPLE_API_PARACOUNT != 3 ) {
+		SIMPLE_API_ERROR(SIMPLE_API_MISS3PARA);
+		return ;
+	}
+	SIMPLE_API_IGNORECPOINTERTYPE ;
+	if ( ! SIMPLE_API_ISNUMBER(1) && ! SIMPLE_API_ISNUMBER(2) ) {
+		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! SIMPLE_API_ISSTRING(3) ) {
+		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
+		return ;
+	}
+	cc_print( (int ) SIMPLE_API_GETNUMBER(1),SIMPLE_API_GETSTRING(2));
+}
 
