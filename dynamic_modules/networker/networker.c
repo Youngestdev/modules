f2316d4de232b3f5d5458cebc35b3995c4d7c6bd
Thecarisma <azeezadewale98@gmail.com>
2018-02-12 02:18:43 +0100

    initialize the string_savant Dynamic libary

---
+++ dynamic_modules/networker/networker.c
@@ -0,0 +1,13 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2018 simple 
+
+*/
+
+/* 
+ * File:   networker.h
+ * Author: thecarisma
+ *
+ * Created on February 12, 2018, 2:17 AM
+ */
---
+++ dynamic_modules/networker/networker.h
@@ -0,0 +1,30 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2018 simple 
+
+*/
+
+/* 
+ * File:   networker.h
+ * Author: thecarisma
+ *
+ * Created on February 12, 2018, 2:17 AM
+ */
+
+#ifndef NETWORKER_H
+#define NETWORKER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NETWORKER_H */
+
