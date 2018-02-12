a0755abd8a39444d4de7e62d8bc0a133e77ce449
Thecarisma <azeezadewale98@gmail.com>
2018-02-12 02:23:48 +0100

    initialize the string_savant Dynamic libary

--- dynamic_modules/networker/networker.c
+++ dynamic_modules/networker/networker.c
@@ -6,8 +6,18 @@
 */
 
 /* 
- * File:   networker.h
+ * File:   networker.c
  * Author: thecarisma
  *
- * Created on February 12, 2018, 2:17 AM
+ * Created on February 12, 2018, 2:18 AM
  */
+
+#include "../../../simple/bootsrc/includes/simple.h"
+#include "networker.h"
+
+SIMPLE_API __declspec(dllexport)
+
+SIMPLE_API void init_simple_module(SimpleState *sState)
+{   
+    //register_block("__printwfb",print_with_foreground_background);
+}
