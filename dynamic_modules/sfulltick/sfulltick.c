fb93ba266c9531222ad835fa97d56cf3e75fb216
Thecarisma <azeezadewale98@gmail.com>
2018-02-18 20:57:37 +0100

    add the web cgi library

--- dynamic_modules/sfulltick/sfulltick.c
+++ dynamic_modules/sfulltick/sfulltick.c
@@ -21,10 +21,10 @@ SIMPLE_API __declspec(dllexport)
 
 SIMPLE_API void init_simple_module(SimpleState *sState)
 {   
-    register_block("__testfulltick",test_fulltick);
+    //register_block("__testfulltick",test_fulltick);
 }
 
-void test_fulltick ( void *pointer )
+void test_fulltick ( )
 {
-    
+    display_help();
 }
