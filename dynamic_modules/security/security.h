ecfce52cd72118ee418e321e011e925fe109a927
Thecarisma <azeezadewale98@gmail.com>
2018-02-18 11:53:25 +0100

    add the web cgi library

---
+++ dynamic_modules/security/security.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   security.h
+ * Author: thecarisma
+ *
+ * Created on February 18, 2018, 11:53 AM
+ */
+
+#ifndef SECURITY_H
+#define SECURITY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SECURITY_H */
+
