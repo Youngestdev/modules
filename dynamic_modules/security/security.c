5fa88ede292fa09c1c8e6cee7131384c28576f24
Thecarisma <azeezadewale98@gmail.com>
2018-02-18 12:28:25 +0100

    add the web cgi library

--- dynamic_modules/security/security.c
+++ dynamic_modules/security/security.c
@@ -22,3 +22,26 @@ SIMPLE_API void init_simple_module(SimpleState *sState)
 {
     register_block("md5",lib_md5);
 }
+
+void lib_md5 ( void *pPointer )
+{
+	unsigned char digest[MD5_DIGEST_LENGTH]  ;
+	char string[33]  ;
+	int x,nSize  ;
+	char *cInput  ;
+	if ( SIMPLE_API_PARACOUNT != 1 ) {
+		SIMPLE_API_ERROR(SIMPLE_API_MISS1PARA);
+		return ;
+	}
+	if ( SIMPLE_API_ISSTRING(1) ) {
+		cInput = SIMPLE_API_GETSTRING(1) ;
+		nSize = SIMPLE_API_GETSTRINGSIZE(1) ;
+		MD5((unsigned char *) cInput, nSize, (unsigned char *) &digest);
+		for ( x = 0 ; x < 16 ; x++ ) {
+			sprintf( &string[x*2] , "%02x" , (unsigned int) digest[x] ) ;
+		}
+		SIMPLE_API_RETSTRING(string);
+	} else {
+		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
+	}
+}
