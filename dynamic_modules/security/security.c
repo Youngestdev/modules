1bcfb3442b413f4f087fc233fb5842d8a2e41fe4
Thecarisma <azeezadewale98@gmail.com>
2018-02-18 12:43:25 +0100

    add the web cgi library

--- dynamic_modules/security/security.c
+++ dynamic_modules/security/security.c
@@ -13,35 +13,24 @@
  */
 
 #include "../../../simple/bootsrc/includes/simple.h"
-#include "./openssl/md5.h"
 #include "security.h"
 
 SIMPLE_API __declspec(dllexport)
 
 SIMPLE_API void init_simple_module(SimpleState *sState)
 {
-    register_block("md5",lib_md5);
+    register_block("sin",math_sin);
 }
 
-void lib_md5 ( void *pPointer )
+void math_sin ( void *pointer )
 {
-	unsigned char digest[MD5_DIGEST_LENGTH]  ;
-	char string[33]  ;
-	int x,nSize  ;
-	char *cInput  ;
-	if ( SIMPLE_API_PARACOUNT != 1 ) {
-		SIMPLE_API_ERROR(SIMPLE_API_MISS1PARA);
-		return ;
-	}
-	if ( SIMPLE_API_ISSTRING(1) ) {
-		cInput = SIMPLE_API_GETSTRING(1) ;
-		nSize = SIMPLE_API_GETSTRINGSIZE(1) ;
-		MD5((unsigned char *) cInput, nSize, (unsigned char *) &digest);
-		for ( x = 0 ; x < 16 ; x++ ) {
-			sprintf( &string[x*2] , "%02x" , (unsigned int) digest[x] ) ;
-		}
-		SIMPLE_API_RETSTRING(string);
-	} else {
-		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
-	}
+    if ( SIMPLE_API_PARACOUNT != 1 ) {
+            SIMPLE_API_ERROR(SIMPLE_API_MISS1PARA);
+            return ;
+    }
+    if ( SIMPLE_API_ISNUMBER(1) ) {
+            SIMPLE_API_RETNUMBER(sin(SIMPLE_API_GETNUMBER(1)));
+    } else {
+            SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
+    }
 }
