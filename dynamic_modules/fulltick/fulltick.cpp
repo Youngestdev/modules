1efbcee31b9e6c113fbd9727b90a00d35287743f
Thecarisma <azeezadewale98@gmail.com>
2018-02-19 23:34:53 +0100

    initialize the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -190,8 +190,9 @@ SIMPLE_BLOCK(on_click)
 		return ;
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISSTRING(2) ) {
-		Fl_Group *window = (Fl_Group* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		window->copy_label(SIMPLE_API_GETSTRING(2));
+		Fl_Widget *window = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		Fl_Callback *call_back = (Fl_Callback *) SIMPLE_API_GETSTRING(2);
+		window->callback(call_back);
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
