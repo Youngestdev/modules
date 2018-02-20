c40ac14e42bbf706624792cde3e6204f8df075e6
Thecarisma <azeezadewale98@gmail.com>
2018-02-20 14:02:48 +0100

    initialize the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -197,7 +197,7 @@ SIMPLE_BLOCK(on_click)
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISSTRING(2) ) {
 		Fl_Widget *window = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		window->callback(window->callback());
+		window->callback((Fl_Callback *)(SIMPLE_API_GETSTRING(2), window));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
