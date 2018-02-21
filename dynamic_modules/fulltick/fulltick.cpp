aeff8b855b7cce818fc4db36dc0675ef828625e4
Thecarisma <azeezadewale98@gmail.com>
2018-02-21 21:19:57 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -429,7 +429,8 @@ SIMPLE_BLOCK(set_size_with_label)
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISNUMBER(3) ) {
 		Fl_Widget *widget = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		fl_measure("", (int&)SIMPLE_API_GETNUMBER(2),(int&)SIMPLE_API_GETNUMBER(3));
+		fl_measure("Hello Worlder", (int&)SIMPLE_API_GETNUMBER(2),(int&)SIMPLE_API_GETNUMBER(3));
+		widget->size((int)SIMPLE_API_GETNUMBER(2),(int)SIMPLE_API_GETNUMBER(2));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
