3ecba2240d78956ddc80a335653796ef1a25a757
Thecarisma <azeezadewale98@gmail.com>
2018-02-21 21:34:56 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -423,15 +423,15 @@ SIMPLE_BLOCK(set_box_type)
 
 SIMPLE_BLOCK(set_size_with_label)
 {
-	if ( SIMPLE_API_PARACOUNT != 3 ) {
-		SIMPLE_API_ERROR(FULLTICK_MISING3PARAM);
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
 		return ;
 	}
-	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISNUMBER(3) ) {
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
 		Fl_Widget *widget = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
 		int wi=0, hi=0; fl_font(widget->labelfont(), widget->labelsize()); 
 		fl_measure(widget->label(), wi,hi);
-		widget->size(wi+5,hi+5);
+		widget->size(wi+(int)SIMPLE_API_GETNUMBER(2),hi+(int)SIMPLE_API_GETNUMBER(2));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
