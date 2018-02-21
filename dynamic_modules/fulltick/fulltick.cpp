b763a669f0cb533f9acfa5eb8ae4e4e0e9c11f36
Thecarisma <azeezadewale98@gmail.com>
2018-02-21 23:19:57 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -131,8 +131,8 @@ SIMPLE_BLOCK(set_label_type)
 		return ;
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
-		Fl_Widget *window = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		window->labeltype((Fl_Labeltype)(int)SIMPLE_API_GETNUMBER(2));
+		Fl_Widget *widget = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		widget->labeltype((Fl_Labeltype)(int)SIMPLE_API_GETNUMBER(2));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
