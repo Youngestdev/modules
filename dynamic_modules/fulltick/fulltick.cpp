8468a84e9cdf8f81b5770e765f5c55284489fa05
Thecarisma <azeezadewale98@gmail.com>
2018-02-22 12:42:41 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -223,11 +223,10 @@ SIMPLE_BLOCK(set_visibility)
 }
 
 void execute_click(Fl_Widget *, void * block) {
-	
-}
-
-void doo(){
-	SIMPLE_API_RETNUMBER(Fl::event_button());
+	printf("CALL BLOCK : %s\n", "blocker");
+	//SimpleState *sState = init_simple_state();
+	//execute_simple_code(sState, (char *) block);
+	//free_simple_state(sState);
 }
 
 /** on click / callback failing **/
@@ -239,9 +238,8 @@ SIMPLE_BLOCK(on_click)
 	}
 	if ( SIMPLE_API_ISCPOINTER(1) ) {
 		Fl_Widget *window = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		window->callback(execute_click);
-		//printf("CALL BLOCK : MOUSE : %i - X %i - Y %i\n", Fl::event_button(), Fl::event_x(), Fl::event_y());
-		
+		window->callback((Fl_Callback *)SIMPLE_API_GETSTRING(2));
+		SIMPLE_API_RETNUMBER(Fl::event());
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
