6391c70e0bdcbfdb41d7b80a89257c2419667660
Thecarisma <azeezadewale98@gmail.com>
2018-02-22 15:15:48 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -228,7 +228,7 @@ SIMPLE_BLOCK(on_click)
 	}
 	if ( SIMPLE_API_ISCPOINTER(1) ) {
 		Fl_Widget *window = (Fl_Widget* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		window->callback(FULLTICK_CALLBACK(SIMPLE_API_GETSTRING(2)));
+		window->callback((Fl_Callback*)SIMPLE_BLOCK(des));
 		
 		//SIMPLE_API_RETNUMBER(handle(Fl::event_button()));
 	} else {
