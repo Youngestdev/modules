9cefa01fc1bbe6be6de5cb6843f12071106c8418
Thecarisma <azeezadewale98@gmail.com>
2018-02-23 10:11:47 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -231,7 +231,7 @@ void test_click(Fl_Widget*, void* callback_struct) {
 	/*if ( simple_vm_eval((VM *) cbs->pointer,cbs->block->str) == 0 ) {
 		((VM *) cbs->pointer)->nEvalCalledFromSimpleCode = 0 ;
 	}*/
-	//simple_string_delete_gc(((VM *) cbs->pointer)->sState,cbs->block);
+	simple_string_delete_gc(((VM *) cbs->pointer)->sState,cbs->block);
 }
 
 /** on click / callback failing **/
