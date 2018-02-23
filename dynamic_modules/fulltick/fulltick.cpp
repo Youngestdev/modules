bb2b7d04e9382a3cf37ce7a5f68b36fff3231346
Thecarisma <azeezadewale98@gmail.com>
2018-02-23 23:31:41 +0100

    add more widget to the GUI(fulltick) module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -554,6 +554,20 @@ SIMPLE_BLOCK(set_button_down_box)
 	}
 }
 
+SIMPLE_BLOCK(set_button_down_color)
+{
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
+		return ;
+	}
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
+		Fl_Button *button = (Fl_Button* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		button->down_color(((Fl_Color) SIMPLE_API_GETNUMBER(2)));
+	} else {
+		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
+	}
+}
+
 SIMPLE_API void init_full_tick(SimpleState *sState) 
 {
 	register_block("__test_gui",test_gui);
@@ -602,5 +616,6 @@ SIMPLE_API void init_full_tick(SimpleState *sState)
 	register_block("__init_round_button",init_round_button);
 	register_block("__init_check_button",init_check_button);
 	register_block("__set_button_down_box",set_button_down_box);
+	register_block("__set_button_down_color",set_button_down_color);
 
 }
