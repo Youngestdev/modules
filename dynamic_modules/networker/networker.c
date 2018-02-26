fd1ec0c2fe5a1c2ab900e7ea90dc08d04a0b89b1
Thecarisma <azeezadewale98@gmail.com>
2018-02-26 16:24:47 +0100

    create the simple/internet module

--- dynamic_modules/networker/networker.c
+++ dynamic_modules/networker/networker.c
@@ -104,12 +104,12 @@ void curl_setopt ( void *pointer )
         return ;
     }
     SIMPLE_API_IGNORECPOINTERTYPE ;
-    if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISNUMBER(3)) {
+    if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISNUMBER(3) && !SIMPLE_API_ISNUMBER(4)) {
         CURLcode *curl_code ; 
         curl_code = (CURLcode *) simple_state_malloc(((VM *) pointer)->sState,sizeof(CURLcode)) ;
         *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), (int) SIMPLE_API_GETNUMBER(3));
         SIMPLE_API_RETCPOINTER(curl_code,"SIMPLE_CURL_CODE");
-    } else if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISSTRING(3)) {
+    } else if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISSTRING(3) && !SIMPLE_API_ISNUMBER(4)) {
         CURLcode *curl_code ; 
         curl_code = (CURLcode *) simple_state_malloc(((VM *) pointer)->sState,sizeof(CURLcode)) ;
         *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), SIMPLE_API_GETSTRING(3));
@@ -117,13 +117,13 @@ void curl_setopt ( void *pointer )
     } else if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISPOINTER(3) && SIMPLE_API_ISNUMBER(4) ) {
         CURLcode *curl_code ; 
         curl_code = (CURLcode *) simple_state_malloc(((VM *) pointer)->sState,sizeof(CURLcode)) ;
-        *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), (void *) SIMPLE_API_GETCPOINTER(3,"void"));
-        SIMPLE_API_RETCPOINTER(curl_code,"SIMPLE_CURL_CODE");
-    } else if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISPOINTER(3) && SIMPLE_API_ISNUMBER(4) ) {
+        *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), (CURL_LIST *) SIMPLE_API_GETCPOINTER(3,"SIMPLE_CURL_LIST"));
+        printf("IT REACH HERE P \n"); SIMPLE_API_RETCPOINTER(curl_code,"SIMPLE_CURL_CODE");
+    } else if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) && SIMPLE_API_ISPOINTER(3) ) {
         CURLcode *curl_code ; 
         curl_code = (CURLcode *) simple_state_malloc(((VM *) pointer)->sState,sizeof(CURLcode)) ;
-        *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), (CURL_LIST *) SIMPLE_API_GETCPOINTER(3,"SIMPLE_CURL_LIST"));
-        SIMPLE_API_RETCPOINTER(curl_code,"SIMPLE_CURL_CODE");
+        *curl_code = curl_easy_setopt((CURL *) SIMPLE_API_GETCPOINTER(1,"SIMPLE_CURL"), (CURLoption ) SIMPLE_API_GETNUMBER(2), (void *) SIMPLE_API_GETCPOINTER(3,"void")); 
+        printf("IT REACH HERE PP\n"); SIMPLE_API_RETCPOINTER(curl_code,"SIMPLE_CURL_CODE");
     } else {
         SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
         return ;
