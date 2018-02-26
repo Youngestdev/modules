e7065bcde77abfbed01c423cf34e5edfcd8967b7
Thecarisma <azeezadewale98@gmail.com>
2018-02-26 18:59:48 +0100

    create the simple/internet module

---
+++ dynamic_modules/parser/parser.c
@@ -0,0 +1,23 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2018 simple 
+
+*/
+
+/* 
+ * File:   parser.c
+ * Author: Azeez Adewale
+ *
+ * Created on February 26, 2018, 6:53 PM
+ */
+
+#include "../../../simple/bootsrc/includes/simple.h"
+#include "parser.h"
+
+SIMPLE_API __declspec(dllexport)
+
+SIMPLE_API void init_simple_module(SimpleState *sState)
+{   
+    //register_block("sin",math_sin);
+}
---
+++ dynamic_modules/parser/parser.h
@@ -0,0 +1,30 @@
+
+/* 
+    Copyright (c) 2016-2018 Azeez Adewale <azeezadewale98@gmail.com"> 
+    MIT License Copyright (c) 2018 simple 
+
+*/
+
+/* 
+ * File:   parser.h
+ * Author: thecarisma
+ *
+ * Created on February 26, 2018, 6:55 PM
+ */
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PARSER_H */
+
