916843be26627062ffe0d00b8427eb138b6a6738
Thecarisma <azeezadewale98@gmail.com>
2018-03-02 02:20:35 +0100

    create the simple/utilities/parser module

--- dynamic_modules/parser/parser.h
+++ dynamic_modules/parser/parser.h
@@ -27,7 +27,7 @@ void init_json_c ( void *pointer ) ;
 void json_c_object_get_type ( void *pointer ) ;
 void json_c_type_to_name ( void *pointer ) ;
 void json_c_object_object_get ( void *pointer ) ;
-void json_c_object_get_string ( void *pointer ) ;
+void json_c_object_to_json_string ( void *pointer ) ;
 
 
 #ifdef __cplusplus
