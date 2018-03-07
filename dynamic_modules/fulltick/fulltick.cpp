e7f7aa8b04f77bc3359387f8f0406da22dbc5e59
Thecarisma <azeezadewale98@gmail.com>
2018-03-07 11:24:47 +0100

    includes the add replace() block to FInput Widget

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -901,6 +901,20 @@ SIMPLE_BLOCK(input_size)
 	}
 }
 
+SIMPLE_BLOCK(input_static_value)
+{
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
+		return ;
+	}
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISSTRING(2) ) {
+		Fl_Input_ *input = (Fl_Input_* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		input->static_value(SIMPLE_API_GETSTRING(2));		
+	} else {
+		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
+	}
+}
+
 SIMPLE_API void init_full_tick(SimpleState *sState) 
 {
 	register_block("__test_gui",test_gui);
@@ -973,5 +987,6 @@ SIMPLE_API void init_full_tick(SimpleState *sState)
 	register_block("__input_replace",input_replace);
 	register_block("__input_shortcut",input_shortcut);
 	register_block("__input_size",input_size);
+	register_block("__input_static_value",input_static_value);
 
 }
