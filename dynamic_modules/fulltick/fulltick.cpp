1634fddbe6b99f54f9ae081ad96a4fa0247fcd3a
Thecarisma <azeezadewale98@gmail.com>
2018-03-08 12:03:40 +0100

    include more properties,features and blocks to FInput Widget

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -989,6 +989,20 @@ SIMPLE_BLOCK(input_value)
 	}
 }
 
+SIMPLE_BLOCK(input_undo)
+{
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
+		return ;
+	}
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
+		Fl_Input_ *input = (Fl_Input_* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		input->textsize((Fl_Fontsize)SIMPLE_API_GETNUMBER(2));
+	} else {
+		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
+	}
+}
+
 SIMPLE_API void init_full_tick(SimpleState *sState) 
 {
 	register_block("__test_gui",test_gui);
@@ -1067,5 +1081,6 @@ SIMPLE_API void init_full_tick(SimpleState *sState)
 	register_block("__input_text_font",input_text_font);
 	register_block("__input_text_size",input_text_size);
 	register_block("__input_value",input_value);
+	register_block("__input_undo",input_undo);
 
 }
