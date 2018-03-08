c0e1e3630987c2f5a19ebc296a49111bf43c948f
Thecarisma <azeezadewale98@gmail.com>
2018-03-08 11:13:38 +0100

    include more properties,features and blocks to FInput Widget

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -951,7 +951,7 @@ SIMPLE_BLOCK(input_text_font)
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
 		Fl_Input_ *input = (Fl_Input_* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
-		input->labelfont((int)SIMPLE_API_GETNUMBER(2));
+		input->labelfont((Fl_Font)SIMPLE_API_GETNUMBER(2));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
 	}
