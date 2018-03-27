705fbf3ba48686a3cc2c548f1eb4b1df138de038
Thecarisma <azeezadewale98@gmail.com>
2018-03-27 14:06:19 +0100

    init the FFileBrowser class in the FULLTICK module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -1110,6 +1110,34 @@ SIMPLE_BLOCK(file_browser_type)
 	}
 }
 
+SIMPLE_BLOCK(file_browser_value)
+{
+	if ( SIMPLE_API_PARACOUNT != 1 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING1PARAM);
+		return ;
+	}
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
+		Fl_File_Browser *widget = (Fl_File_Browser* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		SIMPLE_API_RETNUMBER(widget->value());
+	} else {
+		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
+	}
+}
+
+SIMPLE_BLOCK(file_browser_text)
+{
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
+		return ;
+	}
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
+		Fl_File_Browser *widget = (Fl_File_Browser* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		SIMPLE_API_RETSTRING(widget->text((int)SIMPLE_API_GETNUMBER(2)));
+	} else {
+		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
+	}
+}
+
 SIMPLE_API void init_full_tick(SimpleState *sState) 
 {
 	register_block("__test_gui",test_gui);
@@ -1200,5 +1228,7 @@ SIMPLE_API void init_full_tick(SimpleState *sState)
 	register_block("__init_file_browser",init_file_browser);
 	register_block("__file_browser_load",file_browser_load);
 	register_block("__file_browser_type",file_browser_type);
+	register_block("__file_browser_value",file_browser_value);
+	register_block("__file_browser_text",file_browser_text);
 
 }
