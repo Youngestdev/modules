8c0914224d106f89c1969b8b0a8e6916707f15a1
Thecarisma <azeezadewale98@gmail.com>
2018-03-27 15:41:18 +0100

    init the FFileBrowser class in the FULLTICK module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -1089,7 +1089,7 @@ SIMPLE_BLOCK(file_browser_load)
 		return ;
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISSTRING(2) ) {
-		Fl_Browser *widget = (Fl_Browser* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
+		Fl_File_Browser *widget = (Fl_File_Browser* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_");
 		widget->load(SIMPLE_API_GETSTRING(2));
 	} else {
 		SIMPLE_API_ERROR(FULLTICK_WRONGPARAM);
