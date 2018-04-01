9cff57eb9ecd1db88c7a1c626e9bade3d1c030fe
Thecarisma <azeezadewale98@gmail.com>
2018-04-01 20:55:54 +0100

    modify all the menu widgets in the FULLTICK module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -1196,21 +1196,21 @@ SIMPLE_BLOCK(menu_last_selected_menu_item)
 	}
 	if ( SIMPLE_API_ISPOINTER(1) ) {
 		Fl_Menu_ *menu = (Fl_Menu_* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_"); 
-		SIMPLE_API_RETCPOINTER((Fl_Menu_Item*)menu->picked(),"SIMPLE_FLTK_");
+		SIMPLE_API_RETCPOINTER((Fl_Menu_Item*)menu->mvalue(),"SIMPLE_FLTK_");
 	} else {
 		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
 	}
 }
 
-SIMPLE_BLOCK(menu_global)
+SIMPLE_BLOCK(menu_remove)
 {
-	if ( SIMPLE_API_PARACOUNT != 1 ) {
-		SIMPLE_API_ERROR(FULLTICK_MISING1PARAM);
+	if ( SIMPLE_API_PARACOUNT != 2 ) {
+		SIMPLE_API_ERROR(FULLTICK_MISING2PARAM);
 		return ;
 	}
-	if ( SIMPLE_API_ISPOINTER(1) ) {
+	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISNUMBER(2) ) {
 		Fl_Menu_ *menu = (Fl_Menu_* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_"); 
-		menu->global();
+		menu->remove((int)SIMPLE_API_GETNUMBER(2));
 	} else {
 		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
 	}
