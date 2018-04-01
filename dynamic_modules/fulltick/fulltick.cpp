e4fd2ee407aed5bf3449cdf9e4545cc5c9c30197
Thecarisma <azeezadewale98@gmail.com>
2018-04-01 19:31:01 +0100

    modify all the menu widgets in the FULLTICK module

--- dynamic_modules/fulltick/fulltick.cpp
+++ dynamic_modules/fulltick/fulltick.cpp
@@ -1119,7 +1119,7 @@ SIMPLE_BLOCK(menu_find_menu_item)
 	}
 	if ( SIMPLE_API_ISPOINTER(1) && SIMPLE_API_ISSTRING(2) ) {
 		Fl_Menu_Bar *menu = (Fl_Menu_Bar* ) SIMPLE_API_GETCPOINTER(1,"SIMPLE_FLTK_"); 
-		Fl_Menu_Item *menu_item = (Fl_Menu_Item*)menu->find_item("File") ;
+		Fl_Menu_Item *menu_item = (Fl_Menu_Item*)menu->find_item(SIMPLE_API_GETSTRING(2)) ;
 		SIMPLE_API_RETCPOINTER(menu_item,"SIMPLE_FLTK_");
 	} else {
 		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
