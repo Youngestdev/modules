4a265d5244fc866aaa39ae766a34e02df70771fb
Thecarisma <azeezadewale98@gmail.com>
2018-04-08 23:32:34 +0100

    modify file_savant dynamic module

--- dynamic_modules/file_savant/file_savant.c
+++ dynamic_modules/file_savant/file_savant.c
@@ -95,15 +95,13 @@ void file_exists ( void *pointer )
 	}
 	if ( SIMPLE_API_ISSTRING(1) ) {
             FILE *fp  ;
-            fp = fopen(SIMPLE_API_GETSTRING(1) , "r" );
+            fp = fopen("C:\\Users\\thecarisma\\Documents\\SIMPLE GITHUB\\backup\\smake\\helloworld.sim" , "r" );
             if ( fp ) {
                 fclose( fp ) ;
                 SIMPLE_API_RETNUMBER(1);
-            } else {
-                fclose( fp ) ;
-                SIMPLE_API_RETNUMBER(0);
             }
-            //SIMPLE_API_RETNUMBER(simple_fexists(SIMPLE_API_GETSTRING(1)));
+            fclose( fp ) ;
+            SIMPLE_API_RETNUMBER(0);
 	} else {
 		SIMPLE_API_ERROR(SIMPLE_API_BADPARATYPE);
 	}
