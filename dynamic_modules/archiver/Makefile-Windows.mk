9dd48eb32afcb75e8042bede9a30e05bb410e989
Thecarisma <azeezadewale98@gmail.com>
2018-04-10 19:57:16 +0100

    create (mingw(gcc)) makefile for use on windows

--- dynamic_modules/archiver/Makefile-Windows.mk
+++ dynamic_modules/archiver/Makefile-Windows.mk
@@ -35,7 +35,8 @@ CND_PLATFORM=Windows
 CND_DLIB_EXT=dll
 CND_BUILDDIR=build
 CND_DISTDIR=../../dist
-CLEAN_DEBUGDIR = ../../../s0.3.302-debug
+CLEAN_DEBUGDIR=../../../s0.3.302-debug
+DYNAMIC_MODULEDIR=modules/dynamic_modules
 
 # Object Directory
 OBJECTDIR=${CND_DISTDIR}/${CND_BUILDDIR}/${CND_PLATFORM}
@@ -52,9 +53,9 @@ LDLIBSOPTIONS=../../../simple/src/dist/simple.dll
 
 ${CND_DISTDIR}/${CND_PLATFORM}/archiver.${CND_DLIB_EXT}: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_PLATFORM}
-	${MKDIR} -p {CLEAN_DEBUGDIR}
+	${MKDIR} -p ${CLEAN_DEBUGDIR}/${DYNAMIC_MODULEDIR}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_PLATFORM}/archiver.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared
-	${CP} 
+	${CP} ${CND_DISTDIR}/${CND_PLATFORM}/archiver.${CND_DLIB_EXT} ${CLEAN_DEBUGDIR}/${DYNAMIC_MODULEDIR}
 
 ${OBJECTDIR}/archiver.o: archiver.c
 	${MKDIR} -p ${OBJECTDIR}
