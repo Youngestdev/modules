ac0bc73c7f14ce855d77ce3873ca3b5d7c2cb70c
Thecarisma <azeezadewale98@gmail.com>
2018-04-11 05:20:52 +0100

    create linux makefile for use on linux base system

--- dynamic_modules/makefiles/Makefile-Linux.mk
+++ dynamic_modules/makefiles/Makefile-Linux.mk
@@ -30,6 +30,7 @@ MKDIR=mkdir
 CP = cp
 RMDIR=rmdir
 RM=rm
+MAKE=make
 
 # Macros
 CND_PLATFORM=Windows
@@ -39,15 +40,15 @@ CLEAN_DEBUGDIR=../../../s0.3.302-debug
 DYNAMIC_MODULEDIR=.modules/dynamic_modules
 
 clean: 
-	cd ../archiver/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../consoler/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../file_savant/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../mathic/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../string_savant/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../networker/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../parser/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../security/ && $(MAKE) -f Makefile-Windows.mk
-	cd ../systemic/ && $(MAKE) -f Makefile-Windows.mk
+	cd ../archiver/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../consoler/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../file_savant/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../mathic/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../string_savant/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../networker/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../parser/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../security/ && $(MAKE) -f Makefile-Linux.mk
+	cd ../systemic/ && $(MAKE) -f Makefile-Linux.mk
 	
 	${RM} ${CND_DISTDIR}/build/*.o
 	${RM} ${CND_DISTDIR}/build/*.d
@@ -59,18 +60,15 @@ clean:
 	#${CP} ${CND_DISTDIR}/${CND_PLATFORM}/*.${CND_DLIB_EXT} ${CLEAN_DEBUGDIR}/${DYNAMIC_MODULEDIR}
 
 
-#This Makefile-Windows.mk was written in adaptation to the MINGW
-#description using the Netbeans IDE Makefiles as Guide.
-#
 #If this make file does not work on your Windows PC or you use 
 #alternative compiler e.g Visual Studio, Cygwin e.t.c feel free to 
 #write your own Makefile and send us a pull request at 
 #https://github.com/simple-lang/modules . Your make file must follow 
 #the below format for request to be accepted
 #
-#File-Name : Makefile-Windows-{Compiler}-{WindowsVersion}.mk
+#File-Name : Makefile-Linux-{Compiler}-{WindowsVersion}.mk
 #
-#e.g Makefile-Windows-cygwin-Windows8.mk
+#e.g Makefile-Linux-cygwin-Windows8.mk
 #
 #looking forward to your contribution
 #Thank You
