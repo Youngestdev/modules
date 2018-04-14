78e3128351c7697188d4f6818124f0d01925e893
Azeez Adewale <azeezadewale98@gmail.com>
2018-04-14 11:33:51 +0000

    resolve the dynamic modules makefiles for linux

---
+++ dynamic_modules/core_dynamic_module/core_dynamic_module.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   core_dynamic_module.c
+ * Author: thecarisma
+ *
+ * Created on April 14, 2018, 11:31 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
